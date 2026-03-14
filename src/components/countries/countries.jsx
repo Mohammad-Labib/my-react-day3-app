8f7cc40b84ce9c89698564b14caf8ceab1a92900
Mohammad-Labib <friendlabib@gmail.com>
2026-03-15 00:08:29 +0600

    Flags add

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { use } from 'react';
+import Country from '../country/Country';
 
-const countries = () => {
+const Countries = ({countriesPromise}) => {
+    const CountriesData = use(countriesPromise);
+    const countries = CountriesData.countries;
+    console.log(countries);
     return (
         <div>
-            
+            <h1>In the Country: {countries.length}</h1>
+            {
+                countries.map(country => <Country
+                    key={country.cca3.cca3}
+                    country = {country}></Country>)
+            }
         </div>
     );
 };
 
-export default countries;
+export default Countries;
